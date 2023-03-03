1d666309a2c6bd3b8bde7c05d8595545b1d4eefc
Lucia Rigoni Freire <luciarigoni@live.com>
2023-03-03 11:22:59 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/luciarigoni/workshop/mongo/config/Instantiation.java
+++ src/main/java/com/luciarigoni/workshop/mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.luciarigoni.workshop.mongo.domain.Post;
 import com.luciarigoni.workshop.mongo.domain.User;
+import com.luciarigoni.workshop.mongo.dto.AuthorDTO;
 import com.luciarigoni.workshop.mongo.repository.PostRepository;
 import com.luciarigoni.workshop.mongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP, abs.", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje.", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP, abs.", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje.", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
