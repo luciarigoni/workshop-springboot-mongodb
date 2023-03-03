d63da0431ca0c383fa869b0f5df67d26442b59f6
Lucia Rigoni Freire <luciarigoni@live.com>
2023-03-03 13:19:45 -0300

    Consulta simples com @Query

--- src/main/java/com/luciarigoni/workshop/mongo/repository/PostRepository.java
+++ src/main/java/com/luciarigoni/workshop/mongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.luciarigoni.workshop.mongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.luciarigoni.workshop.mongo.domain.Post;
@@ -10,5 +11,7 @@ import com.luciarigoni.workshop.mongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
